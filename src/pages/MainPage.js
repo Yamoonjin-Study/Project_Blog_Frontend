b720f78a9c20bdc727d66d2800a9d5e3690a835a
yamoonjin <koto89246547@gmail.com>
2021-09-30 13:37:13 +0900

    init: project setup

---
+++ src/pages/MainPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Introduction from '../components/Introduction';
+
+const MainPage = () => {
+  return (
+    <div>
+      <Header/>
+      <Introduction/>
+      <Footer/>
+    </div>
+  );
+};
+
+export default MainPage;
