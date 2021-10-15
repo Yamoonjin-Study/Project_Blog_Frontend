5f9bacaf03eaee74209aaf69b8eb17777abe07c1
yamoonjin <koto89246547@gmail.com>
2021-10-15 13:58:59 +0900

    feat: Login Logic Develope

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../assets/css/main.css';
+import { Link } from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 
-const LoginForm = () => {
+const LoginForm = (props) => {
+
+  const [logIn, setLogIn] = useState({
+    email:"",
+    password:""
+  });
+
+  const onChangeValue = (e) => {
+    setLogIn({
+      ...logIn,
+      [e.target.name] : e.target.value
+    });
+  };
+
+  const submitLogIn = (e) => {
+    e.preventDefault();
+    console.log(logIn);
+    fetch("http://localhost:8080/log-in",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json; charset=utf-8"
+      },
+      body:JSON.stringify(logIn)
+    })
+    .then(res => res.json())
+    .then(res=>{
+      console.log(res.responseMessage);
+      if(res.responseMessage !== "Login Fail"){
+        alert(res.username + " 님, 반갑습니다.");
+        props.history.push('/yamoonjin.com');
+      }else{
+        alert("이메일, 비밀번호를 확인해주세요.");
+      }
+    });
+  };
+
   return (
-    <div>
-      로그인 폼입니다.
+    <div className='mainPage'>
+      <div className='section1'>
+        <div className='section1Contents'>
+          <h4 className='mainTitle'>SIGN IN</h4>
+          <div className='divider'></div>
+          <form className='signUpForm' onSubmit={submitLogIn}>
+            <label className='signUpInfo'>ENTER YOUR EMAIL</label>
+            <br />
+            <input id='email' name='email' type='text' className='signUpInput' onChange={onChangeValue}/>
+            <div className='signUpUnderLine'></div>
+            <br />
+            <label className='signUpInfo'>ENTER YOUR PASSWORD</label>
+            <br />
+            <input id='password' name='password' type='password' className='signUpInput' onChange={onChangeValue}/>
+            <div className='signUpUnderLine'></div>
+            <br />
+            <input type='submit' value='Sign In' className='btn2 btnHover'/>
+            <Link to='/yamoonjin.com/signup'><input type='button' value='Sign Up' className='btn2 btnHover'/></Link>
+            <Link to='/yamoonjin.com'><input type='button' value='Cancel' className='btn2 btnHover'/></Link>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default LoginForm;
+export default withRouter(LoginForm);
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,11 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../assets/css/main.css';
+import { Link } from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 
-const SignUpForm = () => {
+const SignUpForm = (props) => {
+
+  const [signUp, setSignUp] = useState({
+    username:"",
+    password:"",
+    passwordCheck:"",
+    nickname:"",
+    email:"",
+    phone:""
+  });
+
+  const onChangeValue = (e) => {
+    setSignUp({
+      ...signUp,
+      [e.target.name] : e.target.value
+    });
+  }
+
+  const submitSignUp = (e) => {
+    e.preventDefault();
+    console.log(signUp);
+    fetch("http://localhost:8080/sign-up",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json; charset=utf-8"
+      },
+      body:JSON.stringify(signUp)
+    })
+    .then(res => {
+      if(res.status === 201){
+        return res.json();
+      }else{
+        return null;
+      }
+    })
+    .then(res=>{
+      if(res !== null){
+        alert("회원가입을 축하합니다.");
+        props.history.push('/yamoonjin.com');
+      }else{
+        alert("회원가입에 실패하였습니다.");
+      }
+    });
+  }
+
   return (
-    <div>
-      싸인업 폼입니다.
+    <div className='mainPage'>
+      <div className='section1'>
+        <div className='section1Contents'>
+          <h4 className='mainTitle'>SIGN UP</h4>
+          <div className='divider'></div>
+          <form className='signUpForm' onSubmit={submitSignUp}>
+            <label className='signUpInfo'>ENTER YOUR USERNAME</label>
+            <br />
+            <input id='username' name='username' type='text' className='signUpInput' onChange={onChangeValue}/>
+            <div className='signUpUnderLine'></div>
+            <br />
+
+            <label className='signUpInfo'>ENTER YOUR PASSWORD</label>
+            <br />
+            <input id='password' name='password' type='password' className='signUpInput' onChange={onChangeValue}/>
+            <div className='signUpUnderLine'></div>
+            <br />
+
+            <label className='signUpInfo'>ENTER YOUR PASSWORD AGAIN</label>
+            <br />
+            <input id='passwordCheck' name='passwordCheck' type='password' className='signUpInput' onChange={onChangeValue}/>
+            <div className='signUpUnderLine'></div>
+            <br />
+
+            <label className='signUpInfo'>ENTER YOUR NICKNAME</label>
+            <br />
+            <input id='nickname' name='nickname' type='text'className='signUpInput' onChange={onChangeValue}/>
+            <div className='signUpUnderLine'></div>
+            <br />
+
+            <label className='signUpInfo'>ENTER YOUR EMAIL</label>
+            <br />
+            <input id='email' name='email' type='email'className='signUpInput' onChange={onChangeValue}/>
+            <div className='signUpUnderLine'></div>
+            <br />
+
+            <label className='signUpInfo'>ENTER YOUR PHONE&nbsp;&nbsp;&nbsp;&nbsp;<i className='signUpExample'>ex) 01012345678</i></label>
+            <br />
+            <input id='phone' name='phone' type='text' className='signUpInput' onChange={onChangeValue}/>
+            <div className='signUpUnderLine'></div>
+            <br />
+
+            <input type='submit' value='Sign Up' className='btn2 btnHover' onChange={onChangeValue}/>
+            <Link to='/yamoonjin.com'><input type='button' value='Cancel' className='btn2 btnHover'/></Link>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default SignUpForm;
+export default withRouter(SignUpForm);
